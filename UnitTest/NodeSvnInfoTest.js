366ce716da5221cfdd5a84cfe605d3807f805947
xautshuanglong <xjshuanglong@126.com>
2016-03-11 15:29:04 +0800

    reset svn.js and modify redisConfig.json

--- UnitTest/NodeSvnInfoTest.js
+++ UnitTest/NodeSvnInfoTest.js
@@ -2,59 +2,57 @@
  * Created by ericjiang on 2016/2/23.
  */
 var fs = require("fs");
-var SVN = require("node.svn");
+//var SVN = require("node.svn");
 
 var log = require("../LogTools.js");
-var spawn = require("child_process").spawn;
-var iconv = require("iconv-lite");
+var spawn = require('child_process').spawn;
 
 exports.initTest = function(){
     var config =
     {
-        "cwd":process.cwd() + "\\..\\doc",
-        "username":"ericjiang",
-        "password":"Shuanglong3"
+        "cwd":process.cwd() + "\\..\\StarPlantClient\\resource",
+        "username":"shuanglong",
+        "password":"shuanglong"
     };
 
-    var svn = new SVN(config, function (err)
-    {
-        if(err !== null)
-        {
-            //log.error(err);
-        }
-    });
-    //svn.info("约瑟精灵需求节点.xlsx",function callBack(err)
-    //{
-    //    log.error(err);
-    //},"e:\\WebSite\\doc");
+    log.debug("cwd:"+config.cwd);
 
-    //var buffer = new Buffer("");
-    var free = spawn("D:\\Program Files\\TortoiseSVN\\bin\\svn.exe",["info","turn.xml"],{cwd:"e:\\WebSite\\client\\Client"},function callBack(err,stdout,stderr)
-    {
-        if(err !== null)
-        {
-            log.debug(err);
-        }
-    });
-    free.stdout.on("data",function(data)
-    {
-        //log.debug("stdout:\n"+data);
-        //if(Buffer.isBuffer(data))
-        //{
-        //    log.info("is buffer");
-        //    buffer.concat(data);
-        //}
-        log.info(iconv.decode(data,"gbk"));
-    });
-
-    //free.stdout.on("end",function(data)
+    //var svn = new SVN(config, function (err)
     //{
-    //    var str = iconv.decode(buffer,"gbk");
-    //    log.debug("end:\n"+str);
+    //    if(err !== null)
+    //    {
+    //        log.error(err);
+    //    }
+    //    else
+    //    {
+    //        svn.info("theme.thm", function (err, info) {
+    //            if(err == null)
+    //            {
+    //                log.info(info);
+    //            }
+    //            else
+    //            {
+    //                log.error(err);
+    //            }
+    //        });
+    //    }
     //});
 
-    free.on("exit",function(code)
-    {
-        log.debug("exit code:"+code);
-    });
+
+    /* ==================== nodejs process testing ================= */
+    //var free = spawn("svn",["info","theme.thm"],{cwd:"E:\\WebSite\\StarPlantClient\\resource"});
+    //free.stdout.on("data",function(data)
+    //{
+    //    console.log("spawn on data:"+data);
+    //});
+    //
+    //free.stdout.on("end",function(data)
+    //{
+    //    console.log("spawn on end");
+    //});
+    //
+    //free.on("exit",function(code)
+    //{
+    //    console.log("spawn on exit");
+    //});
 };
